5275ab33dc4ffec994bca0dad0565b8865503109
falco13 <keanu0608@gmail.com>
2022-12-07 00:27:42 +0200

    release

---
+++ api/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Director(models.Model):
+    name = models.CharField(max_length=20)
+    surname = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=25)
+    year = models.IntegerField()
+    director = models.ForeignKey(Director, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
