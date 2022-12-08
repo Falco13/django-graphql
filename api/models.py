10da8df596858c7a9e8e4000bf9f10174d85759f
falco13 <keanu0608@gmail.com>
2022-12-09 00:39:41 +0200

    fix model movie

--- api/models.py
+++ api/models.py
@@ -12,7 +12,7 @@ class Director(models.Model):
 class Movie(models.Model):
     title = models.CharField(max_length=25)
     year = models.IntegerField()
-    director = models.ForeignKey(Director, on_delete=models.PROTECT)
+    director = models.ForeignKey(Director, on_delete=models.PROTECT, blank=True, null=True)
 
     def __str__(self):
         return self.title
